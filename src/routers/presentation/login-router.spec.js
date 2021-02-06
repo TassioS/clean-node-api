7043b3c319948245f2ad1b0552b415d27c4e57c5
Tassio Silva <tassio.s13@gmail.com>
2021-02-06 12:16:00 -0300

    Garantindo que o loginRouter retorne 500 se o httpRequest ser invalido e corrigindo lintstaged

---
+++ src/routers/presentation/login-router.spec.js
@@ -0,0 +1,53 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest || !httpRequest.body) return { statusCode: 500 }
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('should return 400  if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: '1234'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Router', () => {
+  test('should return 400  if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'anyemail@email.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
+
+describe('Login Router', () => {
+  test('should return 500  if no httpRequest is provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+  })
+})
+
+describe('Login Router', () => {
+  test('should return 500  if no httpRequest has no body', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route({})
+    expect(httpResponse.statusCode).toBe(500)
+  })
+})
